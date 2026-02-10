5f793d689a24260f6c3f8d1ae93d71c90f347fa1
diegorego514-ship-it <diegorego514@gmail.com>
2026-02-10 15:20:42 -0300

    Add functionality for item price calculations

--- stalin de bermuda.py
+++ stalin de bermuda.py
@@ -38,12 +38,23 @@ def num2(preçodebermudas):
 
     num1: 100
     num2: 90
+
 print("stalin comprou as bermudas e teve seu salário subtraído por 10 de 100 que deu 90")
 
 input("Digite aqui o preço subtraído de stalin: $ + num: ")
+
 def num2(preçodearmas):
     num2: 90
     num2: 80
+
 print("stalin comprou as armas que são 10 AK-47 e teve seu salário subtraído por 10.")
 
 input("o preço subtraído de stalin após ele comprar 10 bermudas e 10 AK-47 são: $ + num: ")
+
+def num3(preçodeitems):
+    num3: 80
+    num3: 70
+
+print("O preço subtraído de stalin após ele comprar 10 poções de recuperação e 10 poções de regeneração mágica é de: $ + num: ")
+
+input("Digite o preço subtraído aqui: ")
