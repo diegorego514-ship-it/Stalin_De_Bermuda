b2384780611fb66cb896cab227a745ed9ecbcbca
diegorego514-ship-it <diegorego514@gmail.com>
2026-02-09 23:55:59 -0300

    Add input prompts for bermuda count and pricing

--- stalin de bermuda.py
+++ stalin de bermuda.py
@@ -11,3 +11,9 @@ print("O numero de bermudas que o stalin usa, é: $ + num: 10")
 input("Digite aqui o numero de bermudas que stalin usa: ")
 
 input("numero de bermudas que o stalin usa: $ + num: ")
+
+print("O numero de bermudas para o stalin experimentar na loja de roupas e armas antes de comprar é: ")
+
+input("Digite aqui o numero de bermudas para experimentar antes de comprar: ")
+
+input("O preço de bermudas para comprar é de? $ + num:")
