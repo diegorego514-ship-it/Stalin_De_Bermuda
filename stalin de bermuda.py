e37eb685af423db9d4311f2c642c56185bf2ac5a
diegorego514-ship-it <diegorego514@gmail.com>
2026-02-09 23:19:54 -0300

    Add files via upload

---
+++ stalin de bermuda.py
@@ -0,0 +1,10 @@
+import shelve
+
+def shelve (AK72):
+
+    if shelve in shelve (stalin = 10):
+
+        shelve in shelve (AK72 = 10)
+
+print("O numero de bermudas que o stalin usa, é: $ + num: 10")
+
