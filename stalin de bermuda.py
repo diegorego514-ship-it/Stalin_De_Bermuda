c3e0e4bc6dc7f42a1f057b11f4c6e8ca700d21f6
diegorego514-ship-it <diegorego514@gmail.com>
2026-02-10 00:24:12 -0300

    Add input prompts for Stalin's bermuda shopping

--- stalin de bermuda.py
+++ stalin de bermuda.py
@@ -14,6 +14,14 @@ input("numero de bermudas que o stalin usa: $ + num: ")
 
 print("O numero de bermudas para o stalin experimentar na loja de roupas e armas antes de comprar é: ")
 
+input("Digite o numero de bermudas no qual o stalin irá experimentar antes de comprar aqui, $ + num: ")
+
 input("Digite aqui o numero de bermudas para experimentar antes de comprar: ")
 
 input("O preço de bermudas para comprar é de? $ + num:")
+
+print("O preço subtraido de stalin é: ")
+
+input("Digite aqui o preço subtraido de stalin quando ele comprou as roupas: ")
+
+input("O preço subtraido de stalin em que comprou roupas e armas é de? $ + num: ")
